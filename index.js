d749d501d12b5561548613378acc523e4c7568cc
Anar Guliev <guliev@internet.ru>
2023-06-07 16:23:03 +0300

    create register and login route

--- index.js
+++ index.js
@@ -15,28 +15,80 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if(!user){
+            return req.status(404).json({
+                message: "Пользователь не найден"
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if(!isValidPass){
+            return req.status(404).json({
+                message: "Неверный логин или пароль"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user.id
+            },
+            'secret123',
+            {
+                expiresIn: '30d'
+            }
+        )
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        })
+    } catch (err){
+        res.status(500).json({
+            message: 'Не удалось авторизоваться.'
+        })
+    }
+})
+
 app.post('/auth/register', registerValidator, async (req, res) => {
     try {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
+            console.log('erer')
             return res.status(400).json(errors.array());
         }
 
         const password = req.body.password;
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, salt);
 
         const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
             avatar: req.body.avatar ?? '',
-            passwordHash
+            passwordHash: hash
         });
 
         const user = await doc.save();
 
-        res.json({ success: true, })
+        const token = jwt.sign({
+            _id: user._id,
+        }, 
+        'secret123',
+        {
+            expiresIn: '30d'
+        })
+
+        const { passwordHash, userData } = user._doc;
+
+        return res.json({ ...userData, token })
     } catch (err) {
         res.status(500).json({
             message: 'Не удалось зарегистрироваться'
